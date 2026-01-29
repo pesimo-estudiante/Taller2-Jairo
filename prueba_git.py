99dfbc9c92f3aa18634d9fb4547e7f2791ad1313
Luna Mariana Rojas Alfonso <lm.rojasa11@uniandes.edu.co>
2026-01-28 20:54:15 -0500

    Modificación colaborativa al archivo prueba_git.py

--- prueba_git.py
+++ prueba_git.py
@@ -1,3 +1,8 @@
 print("Hola mundo desde prueba_git.py")
 
 print("Cambio para validar sincronización con repositorio en GitHub")
+
+# Cambio realizado por colaboradora
+# Se agrega esta línea como ejemplo de trabajo colaborativo en Git
+
+print("Este cambio fue realizado por Luna Mariana como colaboradora en una rama independiente")
